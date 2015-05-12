6b673a58d36ae57eeb7b5b64c77cf8966bae5880
AleksaT <aleksa.topalovic015@gmail.com>
2015-05-12 14:46:59 +0200

    Vracen paket teniskiTeren sa klasama

---
+++ src/teniskiTeren/Implementacija.java
@@ -0,0 +1,41 @@
+package teniskiTeren;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+public class Implementacija {
+
+	private static LinkedList<TeniskiTeren> rezervacije = new LinkedList<>();
+	
+	public void rezervisi (String nazivTerena, String tipTerena, GregorianCalendar datum , int sat) {
+		TeniskiTeren t = new TeniskiTeren();
+		t.setNazivTerena(nazivTerena);
+		t.setTipTerena(tipTerena);
+		t.setDatum(datum);
+		t.setSat(sat);
+		
+		if (rezervacije.contains(t))
+			throw new RuntimeException("Postoji u listi");
+		else 
+			rezervacije.add(t);
+		
+	}
+	
+	public void izbrisiRezervaciju (String nazivTerena, String tipTerena, GregorianCalendar datum , int sat) {
+		TeniskiTeren t = new TeniskiTeren();
+		t.setNazivTerena(nazivTerena);
+		t.setTipTerena(tipTerena);
+		t.setDatum(datum);
+		t.setSat(sat);
+		
+		if (!rezervacije.contains(t))
+			rezervacije.remove(t);
+		else 
+			throw new RuntimeException("Rezervacija ne postoji.");
+	}
+	
+	public String vratiTerene(String tip){
+		//Trajko uradi ovo
+		return null;
+	}
+}
