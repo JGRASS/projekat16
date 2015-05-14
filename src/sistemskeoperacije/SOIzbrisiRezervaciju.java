5d0f81d5bbd47fe930d81d136bd17ef0614e1caf
lukicsasa <lukic.sasa11@gmail.com>
2015-05-14 19:28:28 +0200

    Napisani testovi za klasu RezervacioniSistem i promenjena SOIzbrisiRezervaciju

--- src/sistemskeoperacije/SOIzbrisiRezervaciju.java
+++ src/sistemskeoperacije/SOIzbrisiRezervaciju.java
@@ -1,5 +1,6 @@
 package sistemskeoperacije;
 
+import java.util.Date;
 import java.util.LinkedList;
 
 import teniskiTeren.TeniskiTeren;
@@ -10,12 +11,19 @@ public class SOIzbrisiRezervaciju {
 
 public static void izbrisiRezervaciju (TeniskiTeren t, LinkedList<TeniskiTeren> rezervacije) {
 		
-		for (int i = 0; i < rezervacije.size(); i++) {
-			if (t.getTipTerena().equals(rezervacije.get(i).getTipTerena()) && t.getDatum().equals(rezervacije.get(i).getDatum()) && t.getSat() == rezervacije.get(i).getSat())
-				throw new RuntimeException("Rezervacija ne postoji");
+	String tipTerena = t.getTipTerena();
+	Date datum = t.getDatum();
+	int sat = t.getSat();
+
+	for (int i = 0; i < rezervacije.size(); i++) {
+		TeniskiTeren teren  = rezervacije.get(i);
+		if (teren.getTipTerena().equals(tipTerena) && teren.getDatum().equals(datum) && teren.getSat() == sat) {
+			rezervacije.remove(t);
+			return;
 		}
-		
-		rezervacije.remove(t);
-	
 	}
+
+	throw new RuntimeException("Rezervacija ne postoji");
+}
+
 }
---
+++ test/teniskiTeren/RezervacioniSistemTest.java
@@ -0,0 +1,136 @@
+package teniskiTeren;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RezervacioniSistemTest {
+
+	RezervacioniSistem sistem;
+	
+	@Before
+	public void setUp() throws Exception {
+		sistem = new RezervacioniSistem();
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		sistem = null;
+	}
+
+	@Test
+	public void testRezervisi() {
+		Date datum = new Date();
+
+		TeniskiTeren t = new TeniskiTeren();
+		t.setBrojTelefona("1111");
+		t.setImeKorisnika("Pera Zdera");
+		t.setSat(18);
+		t.setTipTerena("sljaka");
+		t.setDatum(datum);
+		
+		sistem.rezervisi(t);
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testRezervisiDuplikat() {
+		Date datum = new Date();
+
+		TeniskiTeren t = new TeniskiTeren();
+		t.setBrojTelefona("1111");
+		t.setImeKorisnika("Pera Zdera");
+		t.setSat(18);
+		t.setTipTerena("sljaka");
+		t.setDatum(datum);
+		
+		sistem.rezervisi(t);
+		
+		TeniskiTeren t1 = new TeniskiTeren();
+		t1.setBrojTelefona("1111");
+		t1.setImeKorisnika("Pera Zdera");
+		t1.setSat(18);
+		t1.setTipTerena("sljaka");
+		t1.setDatum(datum);
+		
+		sistem.rezervisi(t1);
+	}
+
+	@Test
+	public void testIzbrisiRezervaciju() {
+		Date datum = new Date();
+
+		TeniskiTeren t = new TeniskiTeren();
+		t.setBrojTelefona("1111");
+		t.setImeKorisnika("Pera Zdera");
+		t.setSat(18);
+		t.setTipTerena("sljaka");
+		t.setDatum(datum);
+		
+		sistem.rezervisi(t);
+		
+		sistem.izbrisiRezervaciju(t);
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testIzbrisiRezervacijuDuplikat() {
+		Date datum = new Date();
+
+		TeniskiTeren t = new TeniskiTeren();
+		t.setBrojTelefona("1111");
+		t.setImeKorisnika("Pera Zdera");
+		t.setSat(18);
+		t.setTipTerena("sljaka");
+		t.setDatum(datum);
+		
+		sistem.izbrisiRezervaciju(t);
+		
+		TeniskiTeren t1 = new TeniskiTeren();
+		t1.setBrojTelefona("1111");
+		t1.setImeKorisnika("Pera Zdera");
+		t1.setSat(18);
+		t1.setTipTerena("sljaka");
+		t1.setDatum(datum);
+		
+		sistem.izbrisiRezervaciju(t1);
+	}
+	
+
+	@Test
+	public void testDodajTeren() {
+		String teniskiTeren = "trava";
+		
+		sistem.dodajTeren(teniskiTeren);
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testDodajTerenDuplikat() {
+		String teniskiTeren = "trava";
+		
+		sistem.dodajTeren(teniskiTeren);
+		sistem.dodajTeren(teniskiTeren);
+	}
+
+	@Test
+	public void testIzbrisiTeren() {
+		String teniskiTeren = "trava";
+		sistem.dodajTeren(teniskiTeren);
+		sistem.izbrisiTeren(teniskiTeren);
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testIzbrisiTerenDuplikat() {
+		String teniskiTeren = "trava";
+		
+		sistem.dodajTeren(teniskiTeren);
+		sistem.izbrisiTeren(teniskiTeren);
+		sistem.izbrisiTeren(teniskiTeren);
+		
+	}
+	
+
+}
